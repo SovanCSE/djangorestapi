2dac0630b963df9d31c730578b9f84d8827879bf
Sovan Panda <Sovan@factspan.blr>
2020-04-29 12:26:36 +0530

    code chnages

--- employee/urls.py
+++ employee/urls.py
@@ -16,11 +16,21 @@ Including another URLconf
 # from django.contrib import admin
 from django.urls import path
 import employee.views as employee_views
+from django.views.generic import ListView
+from django.contrib.auth.models import User
 
 urlpatterns = [
     path('',employee_views.registraion, name="registration"),
-    path('login/', employee_views.employee_login, name="login"),
+    # path('login/', employee_views.employee_login, name="login"),#function base view
+    path('login/', employee_views.EmployeeLoginView.as_view(), name="login"), #class base view
     path('viewprofile/', employee_views.employee_profile, name="viewprofile"),
     path('logout/', employee_views.employee_logout, name="logout"),
+    # path('employee_list/', employee_views.EmployeeListView.as_view(), name='employee_list'),
+    ##ListView as generic
+    path('employee_list/', employee_views.ListView.as_view(model =User, template_name =
+    'employee/employee_list.html',context_object_name = 'employee_list'), name='employee_list'),
+    ##ListView as generic
+     path('employee_detail/<slug:username>', employee_views.EmployeeDetailView.as_view(),
+          name='employee_detail'),
 
 ]
--- employee/views.py
+++ employee/views.py
@@ -3,11 +3,14 @@ from .forms import (
     EmployeeRegisterFormv1,
     EmployeeRegisterFormv2,
     EmployeeLoginForm )
-# from .models import EmployeeProfile
+from .models import EmployeeProfile
 # from passlib.hash import pbkdf2_sha256
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.views import View
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.models import User
 
 # Create your views here.
 def registraion(request):
@@ -39,6 +42,30 @@ def employee_login(request):
     return render(request, 'employee/login.html', context)
 
 
+class EmployeeLoginView(View):
+    form_class=EmployeeLoginForm
+    model=EmployeeProfile
+    template_name = 'employee/login.html'
+
+    def get(self, request):
+        print("ll", self.model._meta.verbose_name)
+        form = EmployeeLoginForm()
+        return render(request, self.template_name, {'form':form})
+
+    def post(self, request):
+        form = EmployeeLoginForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            print(username, password)
+            user_info = authenticate(request, username=username, password=password)
+            # print("ll", user_info.username)
+            if user_info:
+                login(request, user_info)
+                return redirect('/viewprofile/')
+            else:
+                return render(request, self.template_name, {"error": "login credential is wrong"})
+
 def employee_logout(request):
     logout(request)
     return HttpResponse("<h1>logged out successfuly</h1> <h4 class='display-3'>for sign in your "
@@ -53,4 +80,27 @@ def employee_profile(request):
     return HttpResponse(f"""<h1>Hi this is my profile, My name is {user.username} and salary: 
             {user.employeeprofile.salary}</h1> <h4 class='h5'>for sign out your account click on <a 
             href='/logout/'>Log out</a></h4>""")
+
+
+class EmployeeListView(ListView):
+    model = User
+    template_name = 'employee/employee_list.html'
+    context_object_name = 'employee_list'
+
+class EmployeeDetailView(DetailView):
+    model = User
+    context_object_name = 'emp'
+    template_name = 'employee/employee_detail.html'
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+
+    # def get_object(self):
+    #     key = self.kwargs.get('key')
+    #     return object.get(key=key)
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     # context['now'] = timezone.now()
+    #     #before showing object through html template  if want to mdify then we can do as well
+    #     return context
 
