9668e39976f9dc3c98f742537dadce9c4837b81d
Sovan Panda <Sovan@factspan.blr>
2020-05-01 00:43:25 +0530

    normal api implementation

---
+++ tests/get_api.py
@@ -0,0 +1,11 @@
+import requests
+from django.http import request
+base_url ="http://localhost:8000/api/v1/"
+api_url ="worker_list/"
+full_url=base_url+api_url
+print(full_url)
+resp_data = requests.get(full_url).json()
+print("get resp data",resp_data)
+
+
+
